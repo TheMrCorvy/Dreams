68035c729c2bacb9750912c4e6c2cfa956e123a7
Gonzalo Salvador Corvalán <salvatore.hakase@gmail.com>
2023-06-08 00:00:21 +0000

    replaced the canvas approach for a simplier svg

--- src/components/Armony/index.tsx
+++ src/components/Armony/index.tsx
@@ -2,19 +2,15 @@
 
 import React, { useRef, useEffect, useState } from "react"
 
-import Canvas from "../Canvas"
-
 import useWindowSize from "../../hooks/useWindowSize"
+import Ring from "./Ring"
 
 const Armony = () => {
 	const [width, height] = useWindowSize()
 
 	const parentRef = useRef<HTMLDivElement>(null)
 
-	const [canvasSize, setCanvasSize] = useState({
-		height: 0,
-		width: 0,
-	})
+	const [canvasSize, setCanvasSize] = useState(0)
 
 	useEffect(() => {
 		if (parentRef.current) {
@@ -24,10 +20,7 @@ const Armony = () => {
 			const size = clientW <= clientH ? clientW : clientH
 			// I want this canvas to be a  square
 
-			setCanvasSize({
-				width: size,
-				height: size,
-			})
+			setCanvasSize(size)
 		}
 	}, [width, height])
 
@@ -40,7 +33,22 @@ const Armony = () => {
 				className="border-solid border-t-0 border-2 border-sky-500 rounded-b-xl container h-72 sm:h-96 lg:h-[30rem] xl:h-[33rem] w-96 sm:w-2/3 md:w-3/5 lg:w-4/6 shadow-2xl shadow-sky-800 flex justify-center items-center bg-transparent"
 				ref={parentRef}
 			>
-				<Canvas {...canvasSize} />
+				{/* <Canvas {...canvasSize} /> */}
+				<div
+					id="canvas"
+					className="relative bg-blue-500"
+					style={{
+						width: canvasSize,
+						height: canvasSize,
+					}}
+				>
+					<hr
+						className="w-full bg-gray-100 border-0 absolute rounded h-0.5"
+						style={{ top: "calc(50% - (0.125rem / 2))" }}
+					/>
+
+					<Ring />
+				</div>
 			</div>
 		</section>
 	)
