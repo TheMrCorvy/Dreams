e46b1495768f96ddca299094bd2aa7cecffba936
Gonzalo corvalan <salvatore.hakase@gmail.com>
2023-07-26 19:21:25 -0300

    created basic modal to interact before the sound effects begin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { JSX } from "react"
 
 import Armony from "@/components/Armony"
+import ActivateSound from "@/components/ActivateSound"
 
 export default function Home(): JSX.Element {
 	return (
 		<main className="flex min-h-screen flex-col relative">
 			<Armony />
+			<ActivateSound />
 		</main>
 	)
 }
