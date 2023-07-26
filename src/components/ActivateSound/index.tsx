b60880d8c2c0682615bad67f64ae100094dac577
Gonzalo corvalan <salvatore.hakase@gmail.com>
2023-07-26 19:43:49 -0300

    removed button with incorrect svg

--- src/components/ActivateSound/index.tsx
+++ src/components/ActivateSound/index.tsx
@@ -14,27 +14,6 @@ const ActivateSound: FC = () => {
 								<h3 className="text-xl font-semibold text-gray-900 dark:text-white">
 									Activate Sound Effects?
 								</h3>
-								<button
-									type="button"
-									className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-									onClick={() => setShowModal(false)}
-								>
-									<svg
-										className="w-3 h-3"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 14 14"
-									>
-										<path
-											stroke="currentColor"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											stroke-width="2"
-											d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-										/>
-									</svg>
-									<span className="sr-only">Close modal</span>
-								</button>
 							</div>
 							<div className="p-6 space-y-6">
 								<p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
